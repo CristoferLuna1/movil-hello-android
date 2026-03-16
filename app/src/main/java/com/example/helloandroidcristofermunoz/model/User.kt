3d92914de0292dd13ec47e30790423ed47399e15
Karen27-bel <sirleyacostakaren@gmail.com>
2026-03-16 16:30:39 -0500

    fix: organizacion final de paquetes y archivos de prueba

--- app/src/main/java/com/example/helloandroidcristofermunoz/model/User.kt
+++ app/src/main/java/com/example/helloandroidcristofermunoz/model/User.kt
@@ -3,5 +3,6 @@ package com.example.helloandroidcristofermunoz.model
 data class User(
     val id: Int,
     val name: String,
-    val email: String
+    val email: String,
+    val age: Int
 )
--- app/src/main/java/com/example/helloandroidcristofermunoz/repository/UserRepository.kt
+++ app/src/main/java/com/example/helloandroidcristofermunoz/repository/UserRepository.kt
@@ -5,9 +5,9 @@ import com.example.helloandroidcristofermunoz.model.User
 class UserRepository {
     fun getUsers(): List<User> {
         return listOf(
-            User(1, "Karen Acosta", "karen@example.com"),
-            User(2, "Emersson", "emersson@example.com"),
-            User(3, "Cristofer", "cristofer@example.com")
+            User(1, "Karen Acosta", "karen@example.com", 20),
+            User(2, "Emersson", "emersson@example.com", 22),
+            User(3, "Cristofer", "cristofer@example.com", 21)
         )
     }
 }
