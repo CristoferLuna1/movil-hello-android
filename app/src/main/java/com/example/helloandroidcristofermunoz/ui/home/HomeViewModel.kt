8f2fe7b62c6634bac332c4237474249e71ec1d6c
emerssonforero03-oss <emerssonforero03@gmail.com>
2026-05-22 09:40:54 -0500

    feat: estados visuales en HomeFragment

--- app/src/main/java/com/example/helloandroidcristofermunoz/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/helloandroidcristofermunoz/ui/home/HomeViewModel.kt
@@ -10,19 +10,41 @@ class HomeViewModel : ViewModel() {
 
     private val repository = TransactionRepository()
 
-    private val _transactions =
-        MutableLiveData<List<Transaction>>()
+    private val _transactions = MutableLiveData<List<Transaction>>()
+    val transactions: LiveData<List<Transaction>> = _transactions
 
-    val transactions: LiveData<List<Transaction>>
-        get() = _transactions
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _isError = MutableLiveData<Boolean>()
+    val isError: LiveData<Boolean> = _isError
+
+    private val _isEmpty = MutableLiveData<Boolean>()
+    val isEmpty: LiveData<Boolean> = _isEmpty
 
     init {
         loadTransactions()
     }
 
     private fun loadTransactions() {
+        _isLoading.value = true
+        _isError.value = false
 
-        _transactions.value =
-            repository.getTransactions()
+        try {
+            // Simular carga de datos
+            Thread.sleep(500)
+            
+            val transactionList = repository.getTransactions()
+            _transactions.value = transactionList
+            _isEmpty.value = transactionList.isEmpty()
+            _isLoading.value = false
+        } catch (e: Exception) {
+            _isLoading.value = false
+            _isError.value = true
+        }
+    }
+
+    fun retryLoad() {
+        loadTransactions()
     }
 }
